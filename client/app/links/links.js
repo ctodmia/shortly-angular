eca51f991529c524f98a3312eac40a8e28a9a5e5
ctodmia <ctodmia@gmail.com>
2015-11-06 19:30:53 -0800

    22 successes, linksController adn getlinks completed.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,12 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-});
+  $scope.data = {};
+  $scope.getLinks = function (){
+       Links.getLinks()
+    .then(function(links) {
+      $scope.data.links=links.data;
+    });
+  };
+  $scope.getLinks();
+})
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,5 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  //so we want to enable navigation between links view and shorten view. 
 });
